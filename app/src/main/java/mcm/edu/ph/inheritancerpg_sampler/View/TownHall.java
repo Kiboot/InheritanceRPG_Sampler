72da0542dbb17c68d621db18e9f97fb44dafd755
Kiboot <kevinlee.cardoza@gmail.com>
2022-02-11 09:02:44 +0800

    added new activities

---
+++ app/src/main/java/mcm/edu/ph/inheritancerpg_sampler/View/TownHall.java
@@ -0,0 +1,16 @@
+package mcm.edu.ph.inheritancerpg_sampler.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mcm.edu.ph.inheritancerpg_sampler.R;
+
+public class TownHall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_town_hall);
+    }
+}
