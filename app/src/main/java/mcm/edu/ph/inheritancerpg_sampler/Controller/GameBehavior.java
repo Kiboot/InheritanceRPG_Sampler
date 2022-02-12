3cb1cc074657b8d771bb13e26ed4a48c535c537e
Kiboot <kevinlee.cardoza@gmail.com>
2022-02-12 12:59:05 +0800

    reduce whitespace

--- app/src/main/java/mcm/edu/ph/inheritancerpg_sampler/Controller/GameBehavior.java
+++ app/src/main/java/mcm/edu/ph/inheritancerpg_sampler/Controller/GameBehavior.java
@@ -6,18 +6,11 @@ public class GameBehavior {
 
     Random randomizer = new Random();
 
-
-
-
-
     public GameBehavior(){}
 
     public int attack(int atkMin,int atkMax,double armor){
-
         double damageReductionFactor = 1- ((0.06 * armor)/(1 +(0.06 * armor)));
-
         double result = Double.valueOf(randomizer.nextInt(atkMax - atkMin) + damageReductionFactor * atkMin);
-
         return (int) result;
     }
 
